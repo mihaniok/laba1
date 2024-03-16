8795510318abe48276be9f8496b4dce37bab1fc4
mihaniok 228 <666lapshin12345@gmail.com>
2024-03-16 11:01:17 +0700

    Было добавлена возможность вводить данные с клавиатуры

--- laba1.cpp
+++ laba1.cpp
@@ -4,15 +4,20 @@
 using namespace std;
 
 int main() {
-	int a, b, P, S;
-	double D;
-	a = 10;
-	b = 8;
+	setlocale(LC_ALL, "Russian");
+
+	int a, b, P, S;// P - периметр, S - площадь
+	double D;// D - диагональ
+
+	cout << "Введите длину прямоугольника ";
+	cin >> a;
+	cout << "Введите ширину прямоугольника ";
+	cin >> b;
 	P = 2 * (a + b);
 	S = a * b;
 	D = pow((pow(a, 2) + pow(b, 2)), 0.5);
-	cout << " S = " << S << endl;
-	cout << " P = " << P << endl;
-	cout << " D = " << D << endl;
+	cout << " Площадь прямоугольника = " << S << endl;
+	cout << " Периметр прямоугольника = " << P << endl;
+	cout << " Диагональ прямоугольника = " << D << endl;
 	return 0;
 }
