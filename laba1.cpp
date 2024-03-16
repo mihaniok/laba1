94c95f646170078472e1b315e04654927192e1be
666la <666la@MIHANIOK-PC>
2024-03-16 10:44:12 +0700

    Добавьте файлы проекта.

---
+++ laba1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a, b, P, S;
+	double D;
+	a = 10;
+	b = 8;
+	P = 2 * (a + b);
+	S = a * b;
+	D = pow((pow(a, 2) + pow(b, 2)), 0.5);
+	cout << " S = " << S << endl;
+	cout << " P = " << P << endl;
+	cout << " D = " << D << endl;
+	return 0;
+}
