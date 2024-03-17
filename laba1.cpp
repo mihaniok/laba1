f361b02b1c048e18314d14a635be94fa291b362d
mihaniok <666lapshin12345@gmail.com>
2024-03-17 17:35:49 +0700

    В программу была добавлена функция вычисления парамтеров треугольника

--- laba1.cpp
+++ laba1.cpp
@@ -3,22 +3,76 @@
 
 using namespace std;
 
-int main() {
-	setlocale(LC_ALL, "Russian");
-
-	int a, b, P, S;// P - периметр, S - площадь
-	double D;// D - диагональ
+void prmglnk() {
+	int P, S, a, b; // P - периметр, S - площадь, D - диагональ 
+	double D;
 
 	cout << "Введите длину прямоугольника ";
 	cin >> a;
 	cout << "Введите ширину прямоугольника ";
 	cin >> b;
+
 	P = 2 * (a + b);
 	S = a * b;
 	D = pow((pow(a, 2) + pow(b, 2)), 0.5);
-	cout << " Площадь прямоугольника = " << S << endl;
+
 	cout << " Периметр прямоугольника = " << P << endl;
+	cout << " Площадь прямоугольника = " << S << endl;
 	cout << " Диагональ прямоугольника = " << D << endl;
+}
+
+void trglnk() {
+    int a, b, c, P;
+    double S, p; // P - периметр, S - площадь, p - полупериметр
+
+    cout << "Введите первую сторону треугольника ";
+    cin >> a;
+    cout << "Введите вторую сторону треугольника ";
+    cin >> b;
+    cout << "Введите третью сторону треугольника ";
+    cin >> c;
+
+    if ((a + b > c) && (a + c > b) && (b + c > a))
+    {
+        P = a + b + c;
+        cout << " Периметр прямоугольника = " << P << endl;
+        p = P / 2;
+        S = sqrt(p * (p - a) * (p - b) * (p - c));//формула Герона
+        cout << " Площадь прямоугольника = " << S << endl;
+        if ((a == b) || (b == c) || (a == c))
+        {
+            if ((a == b) && (b == c))
+            {
+                cout << "Частный случай равнобедренности: треугольник равносторонний";
+            }
+            else
+            {
+                cout << "Треугольник равнобедренный";
+            }
+        }
+    }
+    else
+    {
+        cout << "Треугольник не может существовать";
+    }
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int figura;
+	cout << "выберите фигуру: прямоугольник (1) или треугольник (2)" << endl;
+	cin >> figura;
+	switch (figura) {
+	case (1):
+		prmglnk();
+		break;
+	case (2):
+		trglnk();
+		break;
+	default:
+		cout << "вы ввели недопустимое число" << endl;
+		break;
+	}
 	return 0;
 }
 
